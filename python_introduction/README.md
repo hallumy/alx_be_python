8f9e3a3ff28d8129b531d01c67678b5e9d1b2f8a
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-05-21 00:10:32 +0300

    Add a README.md file and a script of basic arithmetic operations

---
+++ python_introduction/README.md
@@ -0,0 +1,9 @@
+# Python Introduction Project
+
+This is a basic Python script written as part of an introduction to Python 
+programming. The script demonstrates simple concepts of basic arithmetics.
+
+# Basic Arithmetic Exercise
+
+Python script that performs basic arithmetic operations with two predefined 
+numbers. 
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+number1 = 10
+number2 = 5
+
+Add = number1 + number2
+Subtract = number1 - number2
+Multiply = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {Add}')
+print(f'Subtraction of {number1} and {number2} is {Subtract}')
+print(f'Multiplication of {number1} and {number2} is {Multiply}')
