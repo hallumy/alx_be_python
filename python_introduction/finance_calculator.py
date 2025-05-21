f6e5ae823cb62c40246f53f46adde49c4bdb7319
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-05-21 21:15:34 +0300

    Updated finance_calculator.py to display currency using "," and .2f

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -10,5 +10,5 @@ monthly_savings = float(monthly_income) - float(monthly_expenses)
 #calculating the annual savings
 projected_savings = float(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
-print(f'Your monthly savings are ${monthly_savings}.')
-print(f'Projected savings after one year, with interest, is: ${projected_savings}.')
+print(f'Your monthly savings are ${monthly_savings:,.2f}.')
+print(f'Projected savings after one year, with interest, is: ${projected_savings:,.2f}.')
