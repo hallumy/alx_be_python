c87d3e4937ac6b5259b722783f7da8403d0a0d7d
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-06-22 21:45:02 +0300

    Add book_class.py README.md files

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 class Book:
+    """
+    A class that has a constructor __init__  that initializes a Book instance
+    with the title, author and year. It has a destructor __del__ that prints
+    a message upon an object deletion
+    """ 
 
     def __init__(self, title: str, author: str, year: int):
         self.title: str = title
@@ -8,7 +13,7 @@ class Book:
         self.year: int = year
 
     def __str__(self):
-        return f"{self.title} by {self.author}, published in str{self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
